0720c1d97eb6f63f87d481a7782c09d1ba44759f
vitinhou9 <135711616+vitinhou9@users.noreply.github.com>
2023-12-03 19:51:16 -0300

    Add files via upload

--- exercicios.js
+++ exercicios.js
@@ -1,51 +1,59 @@
+
 function soma(a, b) {
-  return a+b;
-}
-function multiplicacao(a, b) {
- return a*b;
-}
-function divisao(a, b) {
- return a/b;
-}
-function subtracao(a, b) {
- return a-b;
-}
-function modulo(a, b) {
- return (a%b)
-}
-function max(a, b) {
- if (a>b)
- return (a)
- else if (a<b)
-{return (b)
-}
-function strConcat(a, b) {
-  let concat = (a+" "+b)
- return(concat)
-
- 
-}
-function retornaNumero(numero) {
- if (numero>10){return ("Maior que 10")}
- else if (numero=10) return("Igual a 10")
- else if (numero<10) return("Menor que 10")
-}
-function ehPositivo(num) {
- if(number>0)
-  return (true);
-  else if (number=<0)
-    return (false);
-}
-
-module.exports = 
-{ 
- soma,
- multiplicacao,
- divisao,
- subtracao,
- modulo,
- max,
- strConcat,
- retornaNumero,
- ehPositivo
-};
+  return(a+b)
+  }
+  function multiplicacao(a, b) {
+    return(a*b)
+  }
+  function divisao(a, b) {
+    return(a/b)
+  }
+  function subtracao(a, b) {
+    return(a-b)
+  }
+  
+  function modulo(a,b) {
+    return (a%b)
+  }
+  function max(a, b) {
+    if(a>b)
+    return (a);
+    else if(a<b)
+    return (b)
+  }
+  function strConcat(a, b) {
+    let concat = (a+" "+b)
+   return(concat)
+  }
+  function retornaNumero(numero) {
+   if(numero>10)
+   return("Maior que 10")
+    else if(numero==10)
+   return("Igual a 10")
+  else if (numero<10)
+    return("Menor que 10")
+  }
+  function ehPositivo(num) 
+  {
+  if (num>0)
+  return(true);
+  else if(num<=0)
+  return(false)
+  }
+  
+   
+  
+  
+  module.exports = 
+  { 
+   soma,
+   multiplicacao,
+   divisao,
+   subtracao,
+   modulo,
+   max,
+   strConcat,
+   retornaNumero,
+   ehPositivo
+  };
+  
